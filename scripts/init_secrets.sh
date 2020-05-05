8235dcbec4df2c55360f781c04169da7251b63b8
Einar Bjerve <einar.bjerve@nav.no>
2020-05-05 12:34:41 +0200

    Leser environment fra rett sted under oppstart

--- scripts/init_secrets.sh
+++ scripts/init_secrets.sh
@@ -1,4 +1,4 @@
-export KAFKA_CV_USER=$(cat /secrets/pamimportapi/oracle/credentials/username)
-export KAFKA_CV_PASSWORD=$(cat /secrets/pamimportapi/oracle/credentials/password)export ORACLE_DB_USER=$(cat /secrets/pamimportapi/oracle/credentials/username)
-export KAFKA_BRUKERNOTIFIKASJON_USER=$(cat /secrets/pamimportapi/oracle/credentials/password)export ORACLE_DB_USER=$(cat /secrets/pamimportapi/oracle/credentials/username)
-export KAFKA_BRUKERNOTIFIKASJON_PASSWORD=$(cat /secrets/pamimportapi/oracle/credentials/password)
+export KAFKA_CV_USER=$(cat /serviceuser/dev/srvpambrukernot/username)
+export KAFKA_CV_PASSWORD=$(cat /serviceuser/dev/srvpambrukernot/password)export ORACLE_DB_USER=$(cat /secrets/pamimportapi/oracle/credentials/username)
+export KAFKA_BRUKERNOTIFIKASJON_USER=$(cat /serviceuser/kafka/brukernotifikasjon/password)export ORACLE_DB_USER=$(cat /secrets/pamimportapi/oracle/credentials/username)
+export KAFKA_BRUKERNOTIFIKASJON_PASSWORD=$(cat /serviceuser/kafka/brukernotifikasjon/password)
