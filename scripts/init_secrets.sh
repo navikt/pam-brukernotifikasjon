33968d7ce56bcc00086e991fc5dfe5c05fd5064b
Einar Bjerve <einar.bjerve@nav.no>
2020-05-05 16:02:55 +0200

    Fjerna avhengigheter som rota til logging

--- scripts/init_secrets.sh
+++ scripts/init_secrets.sh
@@ -1,4 +1,4 @@
-export KAFKA_CV_USER=$(cat /serviceuser/dev/srvpambrukernot/username)
-export KAFKA_CV_PASSWORD=$(cat /serviceuser/dev/srvpambrukernot/password)export ORACLE_DB_USER=$(cat /secrets/pamimportapi/oracle/credentials/username)
-export KAFKA_BRUKERNOTIFIKASJON_USER=$(cat /serviceuser/kafka/brukernotifikasjon/password)export ORACLE_DB_USER=$(cat /secrets/pamimportapi/oracle/credentials/username)
+export KAFKA_CV_USER=$(cat /serviceuser/kafka/srvpambrukernot/username)
+export KAFKA_CV_PASSWORD=$(cat /serviceuser/kafka/srvpambrukernot/password)
+export KAFKA_BRUKERNOTIFIKASJON_USER=$(cat /serviceuser/kafka/brukernotifikasjon/password)
 export KAFKA_BRUKERNOTIFIKASJON_PASSWORD=$(cat /serviceuser/kafka/brukernotifikasjon/password)
--- src/main/kotlin/no/nav/cv/infrastructure/ApplicationStatusController.kt
+++ src/main/kotlin/no/nav/cv/infrastructure/ApplicationStatusController.kt
@@ -2,13 +2,17 @@ package no.nav.cv.infrastructure
 
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import org.slf4j.LoggerFactory
 
 
 @Controller("internal")
 class ApplicationStatusController {
 
     @Get("isReady")
-    fun isReady() = "Ready for everything"
+    fun isReady(): String {
+        LoggerFactory.getLogger(ApplicationStatusController::class.java).info("Bla!")
+        return "Ready for everything"
+    }
 
     @Get("isAlive")
     fun isAlive() = "Alive and kicking"
