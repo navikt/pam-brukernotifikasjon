0edaad3efbf3e5dff1fb8ee5ae0d6a9ff9d5314f
Einar Bjerve <einar.bjerve@nav.no>
2020-05-06 13:59:18 +0200

    Retta bruker-ting

--- scripts/init_secrets.sh
+++ scripts/init_secrets.sh
@@ -1,4 +1,4 @@
-export KAFKA_CV_USER=$(cat /serviceuser/kafka/srvpambrukernot/username)
-export KAFKA_CV_PASSWORD=$(cat /serviceuser/kafka/srvpambrukernot/password)
-export KAFKA_BRUKERNOTIFIKASJON_USER=$(cat /serviceuser/kafka/brukernotifikasjon/password)
+export KAFKA_CV_USER=$(cat /serviceuser/kafka/cv/username)
+export KAFKA_CV_PASSWORD=$(cat /serviceuser/kafka/cv/password)
+export KAFKA_BRUKERNOTIFIKASJON_USER=$(cat /serviceuser/kafka/brukernotifikasjon/username)
 export KAFKA_BRUKERNOTIFIKASJON_PASSWORD=$(cat /serviceuser/kafka/brukernotifikasjon/password)
