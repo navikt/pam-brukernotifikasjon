48d981b134929caacc751395b1aee7871b3bd69a
Einar Bjerve <einar.bjerve@nav.no>
2020-05-07 08:00:37 +0200

    Forenklet til servicebruker for app

--- scripts/init_secrets.sh
+++ scripts/init_secrets.sh
@@ -1,4 +1,2 @@
-export KAFKA_CV_USER=$(cat /serviceuser/kafka/cv/username)
-export KAFKA_CV_PASSWORD=$(cat /serviceuser/kafka/cv/password)
-export KAFKA_BRUKERNOTIFIKASJON_USER=$(cat /serviceuser/kafka/brukernotifikasjon/username)
-export KAFKA_BRUKERNOTIFIKASJON_PASSWORD=$(cat /serviceuser/kafka/brukernotifikasjon/password)
+export KAFKA_SERVICE_USER=$(cat /serviceuser/kafka/brukernotifikasjon/username)
+export KAFKA_SERVICE_USER=$(cat /serviceuser/kafka/brukernotifikasjon/password)
