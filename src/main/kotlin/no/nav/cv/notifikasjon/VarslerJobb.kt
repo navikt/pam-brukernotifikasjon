aef3a2edb798b2029c682919e2f5f3002879129e
Einar Bjerve <einar.bjerve@nav.no>
2020-05-22 10:41:37 +0200

    La til schedulert jobb som varsler u-varslede personer

--- src/main/kotlin/no/nav/cv/notifikasjon/VarslerJobb.kt
+++ src/main/kotlin/no/nav/cv/notifikasjon/VarslerJobb.kt
@@ -2,16 +2,20 @@ package no.nav.cv.notifikasjon
 
 import io.micronaut.scheduling.annotation.Scheduled
 import no.nav.cv.person.PersonIdentRepository
+import java.time.ZonedDateTime
 import javax.inject.Singleton
 
 @Singleton
 class VarslerJobb(
-        hendelseService: HendelseService,
-        repository: PersonIdentRepository
+        private val hendelseService: HendelseService,
+        private val repository: StatusRepository
 ) {
 
     @Scheduled(fixedDelay = "15s")
     fun varsle() {
-
+        val skalVarsles = repository.skalVarsles()
+        skalVarsles.forEach {
+            hendelseService.varsleBruker(it.aktorId, ZonedDateTime.now())
+        }
     }
 }
