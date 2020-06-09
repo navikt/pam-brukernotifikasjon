dec08f8134ac5ecbd0ca7eef4e66b9c402f995f0
Einar Bjerve <einar.bjerve@nav.no>
2020-06-09 10:48:00 +0200

    Renamed groupid of oppfolgingsconsumer to restart topic

--- src/main/kotlin/no/nav/cv/event/oppfolgingstatus/OppfolgingsstatusConsumer.kt
+++ src/main/kotlin/no/nav/cv/event/oppfolgingstatus/OppfolgingsstatusConsumer.kt
@@ -11,7 +11,7 @@ import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
 @KafkaListener(
-        groupId = "pam-brukernotifikasjon-oppfolging",
+        groupId = "pam-brukernotifikasjon-oppfolging-v1",
         offsetReset = OffsetReset.EARLIEST
 )
 class OppfolgingsstatusConsumer(
