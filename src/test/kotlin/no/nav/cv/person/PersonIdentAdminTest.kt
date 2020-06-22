1fb18f5de6f52e69c07caab03fba91a1bdc09462
Einar Bjerve <einar.bjerve@nav.no>
2020-06-22 12:49:44 +0200

    Refaktorerte test av admin-grensesnitt slik at tester som er avhenig av ulike property-verdier ligger i separate klasser

--- src/test/kotlin/no/nav/cv/person/PersonIdentAdminTest.kt
+++ src/test/kotlin/no/nav/cv/person/PersonIdentAdminTest.kt
@@ -3,7 +3,6 @@ package no.nav.cv.person
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import io.micronaut.context.annotation.Property
-import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.RxHttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.http.client.exceptions.HttpClientResponseException
@@ -11,6 +10,7 @@ import io.micronaut.test.annotation.MicronautTest
 import io.micronaut.test.annotation.MockBean
 import io.mockk.mockk
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.assertThrows
 import javax.inject.Inject
 
@@ -23,15 +23,6 @@ class PersonIdentAdminTest {
     lateinit var personIdentRepository: PersonIdentRepository
 
     @Test
-    @Property(name="personIdent.admin.enabled", value="disabled")
-    fun `that admin is disabled by property` () {
-        assertThrows<HttpClientResponseException> {
-            client.toBlocking()
-                    .exchange<Any>("/internal/addIdent/123/321")
-        }
-    }
-
-    @Test
     @Property(name="personIdent.admin.enabled", value="enabled")
     fun `that admin is enabled by property` () {
         assertThat(client.toBlocking()
@@ -40,7 +31,25 @@ class PersonIdentAdminTest {
         ).isEqualTo("OK")
     }
 
+    @MockBean(bean = PersonIdentRepository::class)
+    fun personIdentRepository()
+        = mockk<PersonIdentRepository>(relaxed = true)
 
+}
+
+@MicronautTest
+class PersonIdentAdminDisabledTest {
+
+    @Inject @field:Client("/pam-brukernotifikasjon") lateinit var client: RxHttpClient
+
+    @Test
+    @Property(name="personIdent.admin.enabled", value="disabled")
+    fun `that admin is disabled by property` () {
+        assertThrows<HttpClientResponseException> {
+            client.toBlocking()
+                    .exchange<Any>("/internal/addIdent/123/321")
+        }
+    }
 
     @MockBean(bean = PersonIdentRepository::class)
     fun personIdentRepository()
