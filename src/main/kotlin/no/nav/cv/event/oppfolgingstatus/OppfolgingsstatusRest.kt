b4bebab3ad3ebc71f00ea7ef70174a5ecfad1f1b
Einar Bjerve <einar.bjerve@nav.no>
2020-06-25 07:09:14 +0200

    Satt opp environment-variable for å snakke med SecurityTokenService og oppfølgingsfeed. Bygd litt videre på klienten til Magnus

--- src/main/kotlin/no/nav/cv/event/oppfolgingstatus/OppfolgingsstatusRest.kt
+++ src/main/kotlin/no/nav/cv/event/oppfolgingstatus/OppfolgingsstatusRest.kt
@@ -1,47 +1,62 @@
 package no.nav.cv.event.oppfolgingstatus
 
+import io.micronaut.context.annotation.Value
 import io.micronaut.http.client.HttpClient
 import io.micronaut.scheduling.annotation.Scheduled
+import org.json.JSONObject
 import org.slf4j.LoggerFactory
+import java.util.*
 import javax.inject.Inject
 import javax.inject.Singleton
 
+
+private val uninitialized = "uninitialized"
 @Singleton
 class OppfolgingsstatusRest (
-        private val oppfolgingsService: OppfolgingstatusService
+        private val oppfolgingsService: OppfolgingstatusService,
+        private val oppfolgingsStatusFeedClient: OppfolgingsStatusFeedClient,
+        private val securityTokeServiceClient: SecurityTokeServiceClient,
+        @Value("\${oppfolgingstatus.sts.serviceuser.username}") val user: String,
+        @Value("\${oppfolgingstatus.sts.serviceuser.password}") val pw: String
 ) {
     companion object {
         val log = LoggerFactory.getLogger(OppfolgingsstatusRest::class.java)
     }
 
-    @Inject
-    private lateinit var client: HttpClient
+    // /veilarboppfolging/api/feed/oppfolging
+    // /rest/v1/sts/token
 
-    private var oidcToken: String? = null
+//    @Inject
+//    private lateinit var client: HttpClient
 
-    init {
-        refreshToken()
-    }
+    private var oidcToken: String = uninitialized
 
     @Scheduled(fixedDelay = "15s")
     fun hentOppfolgingstatus() {
 
+
         val currentOidcToken = oidcToken
 
-        if(currentOidcToken == null) {
+        if(currentOidcToken == uninitialized) {
             log.error("Mangler OIDC token når vi skal hente oppfølgingsstatus")
             return
         }
 
+
+        val feed = oppfolgingsStatusFeedClient.feed(
+                authorization = "Bearer $oidcToken"
+        )
         // hent nye saker fra rest og spol igjennom med oppfolgingsService.oppdaterStatus()
     }
 
-    @Scheduled(fixedDelay = "3500s")
+    //@Scheduled(fixedDelay = "3500s")
     fun refreshToken() {
-        oidcToken = null
+        val auth = Base64.getEncoder().encodeToString("$user:$pw".toByteArray(Charsets.UTF_8))
+        val response = securityTokeServiceClient.authenticate("Basic $auth")
 
-
-        // token client her
+        JSONObject(response).query("/access_token")
+        oidcToken = JSONObject(response).getString("access_token")
+                ?: uninitialized
     }
 }
 
