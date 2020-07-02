a329ae96367410e743c89d488785ee82cde3df71
Magnus S Espeland <magnus.s.espeland@nav.no>
2020-07-02 12:04:34 +0200

    Bare returner statuser som fremdeles mangler fødselsnummer

--- src/main/kotlin/no/nav/cv/notifikasjon/StatusRepository.kt
+++ src/main/kotlin/no/nav/cv/notifikasjon/StatusRepository.kt
@@ -86,8 +86,11 @@ open class StatusRepository(
     private val statusPerFodselsnummer =
             """
                 SELECT * 
-                FROM STATUS s
-                WHERE s.STATUS = :status
+                FROM STATUS s INNER JOIN 
+                (
+                    SELECT max(s_inner.TIDSPUNKT) tidspunkt, s_inner.AKTOR_ID
+                    FROM STATUS s_inner GROUP BY s_inner.AKTOR_ID
+                ) as s2 on s2.TIDSPUNKT = s.TIDSPUNKT AND s2.AKTOR_ID = s.AKTOR_ID                WHERE s.STATUS = :status
                 AND s.foedselsnummer = :fodselsnummer
                 ORDER BY s.TIDSPUNKT DESC               
                 LIMIT 1000
