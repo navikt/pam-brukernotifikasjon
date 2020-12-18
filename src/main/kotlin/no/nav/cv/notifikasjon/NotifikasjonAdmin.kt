dc00638b132c94e7815ce9f86470eb27f198e6f1
Magnus S Espeland <magnus.s.espeland@nav.no>
2020-12-18 10:16:20 +0100

    Turn off notifikasjons admin and add some relevant logging.

--- src/main/kotlin/no/nav/cv/notifikasjon/NotifikasjonAdmin.kt
+++ src/main/kotlin/no/nav/cv/notifikasjon/NotifikasjonAdmin.kt
@@ -3,6 +3,7 @@ package no.nav.cv.notifikasjon
 import io.micronaut.context.annotation.Value
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import org.slf4j.LoggerFactory
 import java.util.*
 
 
@@ -13,10 +14,17 @@ class NotifikasjonAdmin(
         private val varselPublisher: VarselPublisher
 ) {
 
+    companion object {
+        private val log = LoggerFactory.getLogger(NotifikasjonAdmin::class.java)
+    }
+
+
     @Post("kafka/manuell/varsel/{uuid}/{fnr}", produces = [ "text/plain" ])
     fun varsle(uuid: UUID, fnr: String): String {
 
         check(adminEnabled == "enabled")
+
+        log.info("NotifikasjonAdmin genererer varsel oppgave for uuid $uuid")
 
         varselPublisher.publish(uuid, fnr)
         return "OK"
@@ -26,6 +34,8 @@ class NotifikasjonAdmin(
     fun done(uuid: UUID, fnr: String): String {
 
         check(adminEnabled == "enabled")
+
+        log.info("NotifikasjonAdmin genererer done melding for uuid $uuid")
 
         varselPublisher.done(uuid, fnr)
         return "OK"
