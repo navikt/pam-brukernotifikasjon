4d308c6d0331ef6cb8924906c0023c707c85281c
marthehagen <marthe.hagen@nav.no>
2021-01-29 15:43:20 +0100

    Bruker nå Builder i validering av meldinger som sendes.

--- src/main/kotlin/no/nav/cv/output/BrukernotifikasjonProducer.kt
+++ src/main/kotlin/no/nav/cv/output/BrukernotifikasjonProducer.kt
@@ -4,9 +4,15 @@ import io.micronaut.context.annotation.Value
 import no.nav.brukernotifikasjon.schemas.Done
 import no.nav.brukernotifikasjon.schemas.Nokkel
 import no.nav.brukernotifikasjon.schemas.Oppgave
+import no.nav.brukernotifikasjon.schemas.builders.DoneBuilder
+import no.nav.brukernotifikasjon.schemas.builders.NokkelBuilder
+import no.nav.brukernotifikasjon.schemas.builders.OppgaveBuilder
 import no.nav.cv.notifikasjon.VarselPublisher
 import org.slf4j.LoggerFactory
+import java.net.URL
 import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZonedDateTime
 import java.util.*
 import javax.inject.Singleton
 
@@ -19,33 +25,41 @@ private val log = LoggerFactory.getLogger(BrukernotifikasjonProducer::class.java
 
 @Singleton
 class BrukernotifikasjonProducer(
-        private val brukernotifikasjonClient: BrukernotifikasjonClient,
-        @Value("\${output.melding.link}") private val link: String
+       private val brukernotifikasjonClient: BrukernotifikasjonClient,
+       @Value("\${output.melding.link}") private val link: String
+
 ) : VarselPublisher {
 
     override fun publish(eventId: UUID, foedselsnummer: String) {
         log.info("Publiserer varsel med eventId $eventId")
-        val nokkel = Nokkel(systembruker, eventId.toString())
-        val oppgave = Oppgave(
-                Instant.now().toEpochMilli(),
-                foedselsnummer,
-                grupperingsId,
-                tekst,
-                link,
-                sikkerhetsnivaa
-        )
+        val nokkel = NokkelBuilder()
+                .withSystembruker(systembruker)
+                .withEventId(eventId.toString())
+                .build()
+        val oppgave = OppgaveBuilder()
+                .withTidspunkt(LocalDateTime.now())
+                .withFodselsnummer(foedselsnummer)
+                .withGrupperingsId(grupperingsId)
+                .withTekst(tekst)
+                .withLink(URL(link))
+                .withSikkerhetsnivaa(sikkerhetsnivaa)
+                .build()
+
         brukernotifikasjonClient.publish(nokkel, oppgave)
     }
 
     override fun done(eventId: UUID, foedselsnummer: String) {
         log.info("Publiserer donemelding for eventId $eventId")
-        val nokkel = Nokkel(systembruker, eventId.toString())
-        val done = Done(
-                Instant.now().toEpochMilli(),
-                foedselsnummer,
-                grupperingsId
-        )
+        val nokkel = NokkelBuilder()
+                .withSystembruker(systembruker)
+                .withEventId(eventId.toString())
+                .build()
+        val done = DoneBuilder()
+                .withTidspunkt(LocalDateTime.now())
+                .withFodselsnummer(foedselsnummer)
+                .withGrupperingsId(grupperingsId)
+                .build()
+
         brukernotifikasjonClient.done(nokkel, done)
     }
-
 }
