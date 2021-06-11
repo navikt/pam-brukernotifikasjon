1200288950bc6b28c2fadbf73f00e9b6b33b92d0
Einar Bjerve <einar.bjerve@nav.no>
2021-06-11 12:39:11 +0200

    differnesierer consumer gauges på label istedet for metric name

--- src/main/kotlin/no/nav/cv/infrastructure/kafka/ConsumerStatusMetrics.kt
+++ src/main/kotlin/no/nav/cv/infrastructure/kafka/ConsumerStatusMetrics.kt
@@ -1,6 +1,8 @@
 package no.nav.cv.infrastructure.kafka
 
+import io.micrometer.core.instrument.Gauge
 import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Tag
 import org.springframework.scheduling.annotation.Scheduled
 import java.util.concurrent.atomic.AtomicInteger
 
@@ -10,8 +12,11 @@ class ConsumerStatusMetrics(
         private val meterRegistry: MeterRegistry
 
 ) {
+
+    private val metricName = "pam-brukernotifikasjon_consumer_status"
+
     private val statusGauges: Map<String, AtomicInteger> = consumers
-            .map { it.topic to meterRegistry.gauge(it.topic, AtomicInteger(0))!! }
+            .map { it.topic to meterRegistry.gauge(metricName, listOf(Tag.of("topic", it.topic)), AtomicInteger(0))!! }
             .toMap()
 
 
