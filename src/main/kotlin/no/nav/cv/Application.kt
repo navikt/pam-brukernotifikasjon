3ba13bc63b1ffc28b48fca4f198fe16c7dd9866b
Magnus S Espeland <magnus.s.espeland@nav.no>
2021-07-13 11:51:01 +0200

    Enable JWE Token validation in Application
    Co-authored-by: jjtg-nav <julian.torp@nav.no>

--- src/main/kotlin/no/nav/cv/Application.kt
+++ src/main/kotlin/no/nav/cv/Application.kt
@@ -1,6 +1,7 @@
 package no.nav.cv
 
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock
+import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
@@ -8,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling
 @SpringBootApplication
 @EnableSchedulerLock(defaultLockAtMostFor = "5m")
 @EnableScheduling
+@EnableJwtTokenValidation
 class PamBrukernotifikasjonApplication
 
 fun main(args: Array<String>) {
