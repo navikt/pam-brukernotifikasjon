29822207e1ccfd3318469eb059a7c451a24a89d7
Magnus S Espeland <magnus.s.espeland@nav.no>
2021-07-26 14:28:18 +0200

    Run fnr job only every 5 minutes until we get more optimized query

--- src/main/kotlin/no/nav/cv/personident/FodselsnummerJobb.kt
+++ src/main/kotlin/no/nav/cv/personident/FodselsnummerJobb.kt
@@ -17,7 +17,7 @@ class FodselsnummerJobb(
     }
 
     @SchedulerLock(name = "fodselsnummerjobb")
-    @Scheduled(fixedDelay = 10 * 1000)
+    @Scheduled(fixedDelay = 5 * 60 * 1000)
     fun fyllInnFnr() {
         statusRepository.manglerFodselsnummer()
                 .forEach {
