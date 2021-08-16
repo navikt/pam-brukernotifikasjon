f9af07587a0dfa8187fe86ae233d7c6738f46f84
Magnus S Espeland <magnus.s.espeland@nav.no>
2021-08-16 13:06:11 +0200

    Set the most recent status to not 'ferdig'

--- src/main/resources/databasemigrations/V9__legg_til_status_ferdig_felt.sql
+++ src/main/resources/databasemigrations/V9__legg_til_status_ferdig_felt.sql
@@ -1,2 +1,15 @@
 ALTER TABLE status
 ADD COLUMN ferdig BOOLEAN NOT NULL DEFAULT true;
+
+
+UPDATE status
+SET ferdig = false
+WHERE id IN (
+    SELECT id
+    FROM STATUS s INNER JOIN
+         (
+             SELECT max(s_inner.TIDSPUNKT) tidspunkt, s_inner.AKTOR_ID
+             FROM STATUS s_inner
+             GROUP BY s_inner.AKTOR_ID
+         ) as s2 on s2.TIDSPUNKT = s.TIDSPUNKT AND s2.AKTOR_ID = s.AKTOR_ID
+)
