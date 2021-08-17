ca1dcf72ce579374b92295e897551dda3484b4a0
Magnus S Espeland <magnus.s.espeland@nav.no>
2021-08-17 10:04:12 +0200

    Better to run this update manually

--- src/main/resources/databasemigrations/V9__legg_til_status_ferdig_felt.sql
+++ src/main/resources/databasemigrations/V9__legg_til_status_ferdig_felt.sql
@@ -1,12 +1,12 @@
 ALTER TABLE status
     ADD COLUMN ferdig BOOLEAN NOT NULL DEFAULT true;
 
-
-UPDATE status
-SET ferdig = false
-WHERE id IN (
-    SELECT max(s_inner.id) id
-    FROM STATUS s_inner
-    GROUP BY s_inner.AKTOR_ID
-)
+-- Run this update manually
+-- UPDATE status
+-- SET ferdig = false
+-- WHERE id IN (
+--     SELECT max(s_inner.id) id
+--     FROM STATUS s_inner
+--     GROUP BY s_inner.AKTOR_ID
+-- )
 
