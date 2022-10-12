ea5bc53a9d990aee55bb6220f6e1d5baecd2b987
Oscar Vladau-Husevold <oscar.vladau-husevold@nav.no>
2022-10-12 12:29:18 +0200

    Bumper java og avhengigheter (#39)
    * Bumper Java og flere dependencies
    
    * Bumper H2, wrapper reservert ord 'year' i anførselstegn
    
    * Bumper java-versjon i workflows
    
    * Fikser test som feiler etter oppgradering pga avrunding i nano-sekunder ved parsing av timestamp
    
    * Ignorerer snyk-warnings i H2 og mockserver-netty som kun brukes i tester / lokal kjøring
    
    * Bumper postgres i Spring managed dependencies
    
    * Bumper div test-dependencies
    
    * Fjerner eksplisitt versjonering av logback classic siden det er spring boot managed

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,22 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.5.2"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.5.10"
-    kotlin("plugin.serialization") version "1.5.10"
-    kotlin("plugin.spring") version "1.5.10"
+    id("org.springframework.boot") version "2.7.4"
+    id("io.spring.dependency-management") version "1.0.14.RELEASE"
+    kotlin("jvm") version "1.7.20"
+    kotlin("plugin.serialization") version "1.7.20"
+    kotlin("plugin.spring") version "1.7.20"
+}
+
+ext {
+    set("snakeyaml.version", "1.32") // Spring 2.7.4 uses vulnerable version 1.30. Can possibly be removed on spring bump
+    set("postgresql.version", "42.5.0") // Postgres 42.3.7 has SNYK-JAVA-ORGPOSTGRESQL-2970521 vulnerability. Can possibly be removed when spring is updated.
 }
 
 group = "no.nav.cv"
 version = "0.1"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
@@ -21,55 +27,50 @@ repositories {
 
 object Version {
     val brukernotifikasjoner = "2.5.1"
-    val avro = "1.9.2"
+    val avro = "1.11.1"
     val confluent = "5.3.0"
-    val token_support = "1.3.8"
+    val token_support = "2.1.6"
 }
 
 dependencies {
-    implementation("ch.qos.logback:logback-classic:1.2.3")
-    implementation("com.h2database:h2:1.4.200")
+    implementation("com.h2database:h2:2.1.214")
     implementation("com.github.navikt:brukernotifikasjon-schemas:${Version.brukernotifikasjoner}")
     implementation("io.confluent:kafka-streams-avro-serde:${Version.confluent}")
     implementation("io.micrometer:micrometer-registry-prometheus")
     implementation("javax.annotation:javax.annotation-api")
-    implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:4.12.0")
-    implementation("net.javacrumbs.shedlock:shedlock-spring:4.24.0")
-    implementation("net.logstash.logback:logstash-logback-encoder:6.2")
-    implementation("no.nav:vault-jdbc:1.3.7")
+    implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:4.42.0")
+    implementation("net.javacrumbs.shedlock:shedlock-spring:4.42.0")
+    implementation("net.logstash.logback:logstash-logback-encoder:7.2")
+    implementation("no.nav:vault-jdbc:1.3.10")
     implementation("org.apache.avro:avro:${Version.avro}")
     implementation("org.flywaydb:flyway-core")
     implementation("org.hibernate:hibernate-core")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
-    implementation("org.json:json:20190722")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
+    implementation("org.json:json:20220924")
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
-//    implementation("org.springframework.boot:spring-boot-starter-security")
-
-//    implementation("no.nav.security:token-validation-test-support:${Version.token_support}")
     implementation("no.nav.security:token-validation-spring:${Version.token_support}")
     implementation("no.nav.security:token-client-spring:${Version.token_support}")
-//    implementation("no.nav.security:token-validation-core:${Version.token_support}")
     implementation("javax.inject:javax.inject:1")
 
-    testImplementation("com.ninja-squad:springmockk:3.0.1")
-    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.3.2")
-    testImplementation("io.mockk:mockk:1.9.3")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
+    testImplementation("com.ninja-squad:springmockk:3.1.1")
+    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
+    testImplementation("io.mockk:mockk:1.13.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
         exclude(module = "mockito-core")
     }
-    testImplementation("org.testcontainers:junit-jupiter:1.15.3")
-    testImplementation("org.testcontainers:kafka:1.15.3")
-    testImplementation("org.mock-server:mockserver-netty:5.11.1")
+    testImplementation("org.testcontainers:junit-jupiter:1.17.4")
+    testImplementation("org.testcontainers:kafka:1.17.4")
+    testImplementation("org.mock-server:mockserver-netty:5.14.0")
 
 }
 
@@ -82,10 +83,13 @@ configurations {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+    }
 }
